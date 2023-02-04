63565eb20ccfd68af68cf49b2e8aa4be58baebe5
Thiago <thiagofreitas1106@gmail.com>
2023-02-04 12:24:34 -0300

    title, searchbar, separationline added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
-      <h1>Weather Challenge</h1>
+      <div className="title-container">
+        <h1>Previsão do tempo</h1>
+      </div>
+
+      <div className="searchbar-container">
+        <input
+          id="icon"
+          type="text"
+          placeholder="Insira aqui o nome da cidade"
+        ></input>
+      </div>
+      <hr className="separation-line" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
